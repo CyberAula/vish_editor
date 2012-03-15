dc5bdd507a032fa37ab80b850482239f6aceb504
Daniel Gallego <dgallego@dit.upm.es>
2012-03-15 12:49:41 +0100

    WYSIWYG NicEditor included in the ViSH_Editor to manage text content

--- js/VISH.Editor.js
+++ js/VISH.Editor.js
@@ -10,6 +10,8 @@ VISH.Editor = (function(V,$,undefined){
 	
 	var nextImageId = 0;  //number for next image id and its slider to resize it
 	
+	var myNicEditor; // to manage the NicEditor WYSIWYG
+	
 	/**
 	 * Initializes the VISH editor
 	 * adds the listeners to the click events in the different images and buttons
@@ -25,6 +27,10 @@ VISH.Editor = (function(V,$,undefined){
 		var evt = document.createEvent("Event");
 		evt.initEvent("OURDOMContentLoaded", false, true); // event type,bubbling,cancelable
 		document.dispatchEvent(evt);	
+		
+		// initialize the NicEditor WYSIWYG
+		myNicEditor = new nicEditor();
+        myNicEditor.setPanel('slides_panel');
 	};
 
 	/**
@@ -110,8 +116,6 @@ VISH.Editor = (function(V,$,undefined){
 						element.body   = $(div).find('img').attr('src');
 						element.style  = $(div).find('img').attr('style');
 					}
-					slide.elements.push(element);
-					element = {};
 				}
 			});
 			excursion.slides.push(slide);
@@ -204,46 +208,9 @@ VISH.Editor = (function(V,$,undefined){
 	 */
 	var _launchTextEditor = function(event){
 		params['current_el'].attr('type','text');
-		params['current_el'].html("<textarea id='input' name='input'></textarea><button id='save_wysiwyg'type='button'>Save text</button>");
-		params['current_editor'] = $("#input").cleditor({
-          width:        params['current_el'].width(), // width not including margins, borders or padding
-          height:       params['current_el'].height()-40, // height not including margins, borders or padding
-          controls:     // controls to add to the toolbar
-                        "bold italic underline strikethrough subscript superscript | font size " +
-                        "style | color highlight removeformat | bullets numbering | outdent " +
-                        "indent | alignleft center alignright justify | rule image link unlink ",
-          colors:       // colors in the color popup
-                        "FFF FCC FC9 FF9 FFC 9F9 9FF CFF CCF FCF " +
-                        "CCC F66 F96 FF6 FF3 6F9 3FF 6FF 99F F9F " +
-                        "BBB F00 F90 FC6 FF0 3F3 6CC 3CF 66C C6C " +
-                        "999 C00 F60 FC3 FC0 3C0 0CC 36F 63F C3C " +
-                        "666 900 C60 C93 990 090 399 33F 60C 939 " +
-                        "333 600 930 963 660 060 366 009 339 636 " +
-                        "000 300 630 633 330 030 033 006 309 303",    
-          fonts:        // font names in the font popup
-                        "Arial,Arial Black,Comic Sans MS,Courier New,Narrow,Garamond," +
-                        "Georgia,Impact,Sans Serif,Serif,Tahoma,Trebuchet MS,Verdana",
-          sizes:        // sizes in the font size popup
-                        "1,2,3,4,5,6,7",
-          styles:       // styles in the style popup
-                        [["Paragraph", "<p>"], ["Header 1", "<h1>"], ["Header 2", "<h2>"],
-                        ["Header 3", "<h3>"],  ["Header 4","<h4>"],  ["Header 5","<h5>"],
-                        ["Header 6","<h6>"]],
-          useCSS:       false, // use CSS to style HTML when possible (not supported in ie)
-          docType:      // Document type contained within the editor
-                        '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">',
-          docCSSFile:   // CSS file used to style the document contained within the editor
-                        "", 
-          bodyStyle:    // style to assign to document body contained within the editor
-                        "margin:4px; font:10pt Arial,Verdana; cursor:text"
-        });
-		// when the button is pressed the text and the html code from the wysiwyg editor are saved
-		// TODO: save the text in a JSON file
-		$("#save_wysiwyg").click(function(){
-        	params['current_editor'][0].select();
-        	var text = params['current_editor'][0].selectedText();
-        	var htmlText = params['current_editor'][0].selectedHTML();
-        });
+		var wysiwygId = params['current_el'][0].id;
+		params['current_el'].html("<div class='wysiwygInstance' id=wysiwygId style='width: params['current_el'].width(); height: params['current_el'].height();'>Insert text here</div>");
+        myNicEditor.addInstance(wysiwygId);
 	};
 
 	
