796664e59b8dd368055a3c689feea9b2d66dc20e
Enrique Barra <enrique.barra.arias@gmail.com>
2012-03-15 09:13:19 +0100

    Preparing the gem.

--- rails/app/assets/javascripts/VISH.js
+++ rails/app/assets/javascripts/VISH.js
@@ -6,5 +6,5 @@ VISH.Mods || (VISH.Mods = {});
 VISH.VERSION = '0.1';
 VISH.AUTHORS = 'GING';
 
-VISH.ImagesPath = "/images/";
-VISH.StylesheetsPath = "/stylesheets/";
+VISH.ImagesPath = "/assets/";
+VISH.StylesheetsPath = "/assets/";
