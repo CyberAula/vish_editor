eb15e8e552b6203fcf7126de16935156253a5e14
Enrique Barra <enrique.barra.arias@gmail.com>
2012-03-16 10:51:58 +0100

    Preparing the gem.

--- js/VISH.Editor.js
+++ js/VISH.Editor.js
@@ -153,7 +153,7 @@ VISH.Editor = (function(V,$,undefined){
 		}
 		
 		$.post(initOptions["postPath"], params, function(data) {
-	      	$(this).html(data);
+	      	jQuery(document).html(data);
 	    });
 		
 	};
