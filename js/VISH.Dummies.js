3df26dfefa338fcf01c9a6c411bdefcb5a05ed4f
Enrique Barra <ebarra@dit.upm.es>
2012-03-16 13:15:51 +0100

    background in fancybox

--- js/VISH.Dummies.js
+++ js/VISH.Dummies.js
@@ -6,7 +6,7 @@ VISH.Dummies = (function(VISH,undefined){
 	//array with the articles (slides) definition, one for each template
 	//the ids of each div are id='id_to_change' and will be replaced by the next id by the function _replaceIds(string)
 	var dummies = [
-		"<article id='article_id_to_change' template='t1'><div id='div_id_to_change' areaid='header' class='t1_header editable grey_background'></div><div id='div_id_to_change' areaid='left' class='t1_left editable grey_background'><div class='edit_pencil'><img class='edit_pencil_img' src='/assets/edit.png'/></div></div><div id='div_id_to_change' areaid='right' class='t1_right editable grey_background'></div></article>",
+		"<article id='article_id_to_change' template='t1'><div id='div_id_to_change' areaid='header' class='t1_header editable grey_background'></div><div id='div_id_to_change' areaid='left' class='t1_left editable grey_background'></div><div id='div_id_to_change' areaid='right' class='t1_right editable grey_background'></div></article>",
 		"<article id='article_id_to_change' template='t2'><div id='div_id_to_change' areaid='header' class='t2_header editable grey_background'></div><div id='div_id_to_change' areaid='left' class='t2_left editable grey_background'></div></article>"
 	]; 
 
