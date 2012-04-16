ea3b59e451a55cbaeefa89a3deda7c69a7bf4b39
Daniel Gallego <dgallego@dit.upm.es>
2012-04-16 13:13:18 +0200

    Bug fixed: the Chrome's orange outline related to focused objects has been removed.

--- js/VISH.Editor.Text.js
+++ js/VISH.Editor.Text.js
@@ -11,23 +11,21 @@ VISH.Editor.Text = (function(V,$,undefined){
   * Allows users to include text content in the slide using a WYSIWYG editor
   */
   var launchTextEditor = function(){
-		var current_area = VISH.Editor.getCurrentArea();
+	var current_area = VISH.Editor.getCurrentArea();
+	// only one instance of the NicEditor is created
     if(myNicEditor == null) {
       myNicEditor = new nicEditor();
-          myNicEditor.setPanel('slides_panel');
+      myNicEditor.setPanel('slides_panel');
     }
+    
     current_area.attr('type','text');
     var wysiwygId = "wysiwyg_" + current_area[0].id;
     var wysiwygWidth = current_area.width() - 10;
     var wysiwygHeight = current_area.height() - 10;
     current_area.html("<div class='wysiwygInstance' id="+wysiwygId+" style='width:"+wysiwygWidth+"px; height:"+wysiwygHeight+"px;'>Insert text here</div>");
     myNicEditor.addInstance(wysiwygId);
-    /*$("#"+wysiwygId).keydown(function(e) {
-      if(e.keyCode == 39) {
-          
-      }
-    });*/
-   
+
+	// add a button to delete the current text area   
     V.Editor.addDeleteButton(current_area);
   }
 	
