8f260ba25c0732ecc82859954d181ef2049f6849
Víctor Hugo Bazán <vh.bazan@upm.es>
2012-05-18 18:44:46 +0200

    M Ch quiz: adding M Ch options v 3.0

--- js/VISH.Editor.Quiz.js
+++ js/VISH.Editor.Quiz.js
@@ -3,13 +3,12 @@ VISH.Editor.Quiz = (function(V,$,undefined){
 	//var buttonAddOptionClass = "add_quiz_option"; //con esto sí funciona	
 	var MultipleChoiceoptionClass = "multiplechoice_radio";
 	var searchOptionText= "mchoice_radio_option_";
+	var num_options = 5; // maximum options can be added
 	
-	var number_patern =/[0-9]/;
 	var init = function(){
 		//$(document).on('click','.add_quiz_option', addMultipleChoiceOption);
 		$(document).on('click','#'+buttonAddOptionId , addMultipleChoiceOption);
-		console.log("Entramos en init (Quiz) y buscamos class de a: " + $(".add_quiz_option") );
-		
+			
 	};	
 	/*
 	 Function that add an input radio option for the Multiple Choice Quiz  
@@ -24,13 +23,18 @@ VISH.Editor.Quiz = (function(V,$,undefined){
 		//the input in text type  
 		var text  = $('<div>').append($('.' +MultipleChoiceoptionClass).clone()).html();
 		
-		console.log("text vale:  "+ text);
+		//console.log("text vale:  "+ text);
 		
-		//temp solution  
-		var number = text.substr(32, 1); //TODO find better way to count radio inputs
-		var next_num = parseInt(number)+1; // increase 
-		console.log("number con patrones y match vale:  "+ number );
-		console.log("next_number :  "+ next_num );
+		var total = 0;
+		
+		// get the number of radio inputs 
+	$('.'+MultipleChoiceoptionClass).each(function(i){
+ total = i;
+}); 		
+		var next_num = parseInt(total)+1;
+		if (next_num < num_options) {
+			console.log("next_number :  "+ next_num );
+		  
 		    //the next radio input   
 			var add_option = "<br><input id='mchoice_radio_option_"+next_num+"' type='radio' class='multiplechoice_radio' /><input id='radio_text_"+next_num+"' type='text' placeholder='insert text option here' />";
 			add_option += "<a src='' id='"+buttonAddOptionId+"' class='add_quiz_option'><img src='images/add_quiz_option.png' id='add_quiz_option_img'/> </a>";
@@ -39,9 +43,16 @@ VISH.Editor.Quiz = (function(V,$,undefined){
 			$(".add_quiz_option").remove();
 			//add radio + button 			
 			$(".mcquestion").append(add_option);	
-			//add onclick event to the button + 
-			$(document).on('click','#'+buttonAddOptionId , addMultipleChoiceOption);
-				
+			
+		} else if (next_num = num_options) {
+			
+			var add_option = "<br><input id='mchoice_radio_option_"+next_num+"' type='radio' class='multiplechoice_radio' /><input id='radio_text_"+next_num+"' type='text' placeholder='insert text option here' />";
+			add_option += "<a src='' id='"+buttonAddOptionId+"' class='add_quiz_option'>";
+			
+			$(".add_quiz_option").remove();
+			
+			$(".mcquestion").append(add_option);
+		}
 		
 	};
 			
