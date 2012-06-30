ae94a7a482a7454bbb97a3ba37936a30ff08b60d
Victor H. Bazán <vh.bazan@upm.es>
2012-07-01 01:58:03 +0200

    MCh Quiz: added ids for removing options

--- js/VISH.Editor.Quiz.js
+++ js/VISH.Editor.Quiz.js
@@ -29,27 +29,37 @@ var inputs_search = $(".current").find("."+MultipleChoiceOptionClass);
 
 		if (next_num < num_options) {
 
-		    //the next radio input  
-		    var add_option = "<a href='javascript:VISH.Editor.Quiz.removeMultipleChoiceOption("+(next_num -1)+")' id='"+buttonRemoveOptionId+"' class='remove_quiz_option'><img src='images/delete.png' id='remove_quiz_option_img'/></a>";
-		     
-			add_option += "<br>"+next_index+") <input id='radio_text_"+next_num+"' class='"+MultipleChoiceOptionClass+"' type='text' placeholder='insert text option here' />";
-			add_option += "<a id='"+buttonAddOptionId+"' class='add_quiz_option'><img src='images/add_quiz_option.png' id='add_quiz_option_img'/></a>";
+		    //the next radio input  TODO ... add delete in li and add li with input and letter
+		    $(".add_quiz_option").remove();
+		    
+		    var delete_icon = "<a href='javascript:VISH.Editor.Quiz.removeMultipleChoiceOption("+(next_num -1)+")' id='"+buttonRemoveOptionId+"' class='remove_quiz_option'><img src='images/delete.png' id='remove_quiz_option_img'/></a>";
+		    
+		    $(".current").find("#ul_mch_options").find("#li_mch_option_"+(next_num-1)).append(delete_icon);
+		    var add_option = "<li id='li_mch_option_"+next_num+"'>"+next_index+") <input id='radio_text_"+next_num+"' class='"+MultipleChoiceOptionClass+"' type='text' placeholder='insert text option here' />";
+			//add_option += "<br>"+next_index+") <input id='radio_text_"+next_num+"' class='"+MultipleChoiceOptionClass+"' type='text' placeholder='insert text option here' />";
+			add_option += "<a id='"+buttonAddOptionId+"' class='add_quiz_option'><img src='images/add_quiz_option.png' id='add_quiz_option_img'/></a></li>";
 			
+			$(".current").find("#ul_mch_options").append(add_option);
 			//remove button + 
-			$(".add_quiz_option").remove();
+			
 			//add radio + button 	
-			$(".current").find(".mcquestion").append(add_option);		
+			//$(".current").find(".mcquestion").append(add_option);		
 				
 
 			
 		} else if (next_num = num_options) {
 			
-			 var add_option = "<a href='javascript:VISH.Editor.Quiz.removeMultipleChoiceOption("+(next_num -1)+")' id='"+buttonRemoveOptionId+"' class='remove_quiz_option'><img src='images/delete.png' id='remove_quiz_option_img'/></a>";
-			 add_option += "<br>"+next_index+") <input id='radio_text_"+next_num+"' class='"+MultipleChoiceOptionClass+"' type='text' placeholder='insert text option here' />";
-			 add_option += "<a href='javascript:VISH.Editor.Quiz.removeMultipleChoiceOption("+(next_num)+")' id='"+buttonRemoveOptionId+"' class='remove_quiz_option'><img src='images/delete.png' id='remove_quiz_option_img'/></a>";
 			$(".add_quiz_option").remove();
 			
-			$(".mcquestion").append(add_option);
+			var delete_icon = "<a href='javascript:VISH.Editor.Quiz.removeMultipleChoiceOption("+(next_num -1)+")' id='"+buttonRemoveOptionId+"' class='remove_quiz_option'><img src='images/delete.png' id='remove_quiz_option_img'/></a>";
+			$(".current").find("#ul_mch_options").find("#li_mch_option_"+(next_num-1)).append(delete_icon);
+			
+			// var add_option = "<a href='javascript:VISH.Editor.Quiz.removeMultipleChoiceOption("+(next_num -1)+")' id='"+buttonRemoveOptionId+"' class='remove_quiz_option'><img src='images/delete.png' id='remove_quiz_option_img'/></a>";
+			 var add_option = "<li id='li_mch_option_"+next_num+"'>"+next_index+")&nbsp;  <input id='radio_text_"+next_num+"' class='"+MultipleChoiceOptionClass+"' type='text' placeholder='insert text option here' />";
+			 add_option += "<a href='javascript:VISH.Editor.Quiz.removeMultipleChoiceOption("+(next_num)+")' id='"+buttonRemoveOptionId+"' class='remove_quiz_option'><img src='images/delete.png' id='remove_quiz_option_img'/></a></li>";
+			
+			$(".current").find("#ul_mch_options").append(add_option);
+		//	$(".mcquestion").append(add_option);
 
 		}
 		
