14ff78865696276cda2109532d714c611c30f8a2
Enrique Barra <ebarra@dit.upm.es>
2012-08-15 16:37:22 +0200

    conflict solved

--- controllers/presentation.js
+++ controllers/presentation.js
@@ -13,15 +13,6 @@ var database = require("../db/api");
 var options = JSON.stringify(require('../public/vishEditor/configuration/configuration.js').getOptions());
 
 exports.index = function(req,res){
-<<<<<<< HEAD
-  console.log("Presentation index");
-  res.render('index');
-}
-
-exports.get = function(req,res){
-  console.log("Presentation get");
-  res.render('index');
-=======
   res.redirect('/home');
 }
 
@@ -40,28 +31,18 @@ exports.create = function(req,res){
       res.send(data);
     }
   });
->>>>>>> a15fd47db50b8702c82e27e815db9bbad964bae8
 }
 
 exports.show = function(req,res){
-  console.log("Presentation show");
   var id = req.params.id;
   database.findPresentationById(id,function(err,presentation){
     if(err){
-<<<<<<< HEAD
-      res.render('home');
-    } else {
-      database.findUserById(presentation.author,function(err,user){
-        if((err)||(user===null)){
-          res.render('home');
-=======
       req.flash('warn','Presentation not found');
       res.render('genericError', { locals: {returnUrl: "/home" } });
     } else {
       database.findUserById(presentation.author,function(err,user){
         if((err)||(user===null)){
           res.redirect('/home');
->>>>>>> a15fd47db50b8702c82e27e815db9bbad964bae8
         } else {
           res.render('presentation/show', {locals: {presentation: presentation, author: user.name, options: options}});
         }
@@ -120,29 +101,8 @@ exports.update = function(req,res){
   });
 }
 
-<<<<<<< HEAD
-exports.edit = function(req,res){
-  console.log("Presentation edit");
-  var id = req.params.id;
-  database.findPresentationById(id,function(err,presentation){
-    if(err){
-      res.render('home');
-    } else {
-      database.findUserById(presentation.author,function(err,user){
-        if((err)||(user===null)){
-          res.render('home');
-        } else {
-          res.render('presentation/edit', {locals: {presentation: presentation, author: user.name, options: options}});
-        }
-      });
-    }
-  });
-  res.render('home');
-}
-=======
 exports.destroy = function(req,res){
   var id = req.params.id;
->>>>>>> a15fd47db50b8702c82e27e815db9bbad964bae8
 
   if(req.user.presentations.indexOf(id)===-1){
     //Current user is not the owner of this presentation
