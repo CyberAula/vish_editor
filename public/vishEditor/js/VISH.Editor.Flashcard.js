43d93f6ed2c06d216a709e72e743483f068c6098
Enrique Barra <ebarra@dit.upm.es>
2012-09-04 12:13:09 +0200

    changes for flashcard

--- public/vishEditor/js/VISH.Editor.Flashcard.js
+++ public/vishEditor/js/VISH.Editor.Flashcard.js
@@ -12,19 +12,36 @@ VISH.Editor.Flashcard = (function(V,$,undefined){
 		//hide slides
 		V.Editor.Utils.hideSlides();
 
-		//change thumbnail onclick event (preview slide instead of go to edit it)
-		//it will change itself depending on excursionType
-		V.Editor.Thumbnails.redrawThumbnails();
-
 		//show flashcard background, should be an image with help
 		$("#flashcard-background").show();
+		
+		$( "#flashcard-background" ).droppable();  //to accept the pois
+		$(".carrousel_element_single_row_slides").droppable();
+
+		//change thumbnail onclick event (preview slide instead of go to edit it)
+		//it will change itself depending on excursionType, also remove drag and drop to order slides
+		V.Editor.Thumbnails.redrawThumbnails();
+
+		//redraw the Pois, restore where they were in case we are returning to flashcard
+		_redrawPois();
 
 		//show change background button
 
 		//show draggable items to create the flashcard
 		//ALL THIS ACTIONS WILL HAVE TO BE CALLED AFTER THE THUMBNAILS HAVE BEEN REWRITTEN
 		//var también si se pudiese hacer appendTo al background y así poder calcular facil la posición final
-		$("#poi1").draggable();
+		$("#poi1").draggable({
+			revert: "invalid",   //poi will return to original position if not dropped on the background
+			stop: function(event, ui) { //change the moved attribute of the poi
+				//check if inside background
+				if($(event.srcElement).offset().top > 100 && $(event.srcElement).offset().top < 700 && $(event.srcElement).offset().left > 55 && $(event.srcElement).offset().left < 855){
+					$(event.srcElement).attr("moved", "true");
+				}
+				else{
+					$(event.srcElement).attr("moved", "false");
+				}
+			}
+		});
 		$(".image_carousel").css("overflow", "visible");
 		$("#menubar").css("z-index", "2000");
 		
@@ -32,8 +49,33 @@ VISH.Editor.Flashcard = (function(V,$,undefined){
 		//con eso calculo su posición final en el background
 	};
 
+	var _redrawPois = function(){
+		console.log("_redrawPois");
+	};
+
+	/*
+	 * returns an array of pois
+	 */
+	var savePois = function(){
+		var pois = [];
+		$(".fc_draggable_arrow [moved='true']").each(function(index,s){
+			pois[index]= {};
+			pois[index].id = $(s).attr('id');
+			pois[index].x = $(s).offset().left - 55;
+			pois[index].y = $(s).offset().top - 75;
+			pois[index].slide_id = $(s).attr('slide_id');
+		});
+		return pois;
+	};
+
+	var removePois = function(){
+		$(".fc_draggable_arrow").remove();
+	};
+
 	return {
-		init 				: init
+		init 				: init,
+		removePois			: removePois,
+		savePois			: savePois
 	};
 
 }) (VISH, jQuery);
