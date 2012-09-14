88650f8506fbd6ba0146ceb3ea9b6945217ba995
Enrique Barra <ebarra@dit.upm.es>
2012-09-14 13:52:25 +0200

    flashcard working in mobiles

--- public/vishEditor/js/VISH.Slides.Events.js
+++ public/vishEditor/js/VISH.Slides.Events.js
@@ -65,54 +65,59 @@ VISH.Slides.Events = (function(V,$,undefined){
 
 
 	var handleTouchStart = function(event) {
-	  var touches = getTouches(event);
-	  if (touches.length === 1) {
-	    touchDX = 0;
-	    touchDY = 0;
+	  if(V.ViewerEngine === "presentation"){
+			  var touches = getTouches(event);
+			  if (touches.length === 1) {
+			    touchDX = 0;
+			    touchDY = 0;
 
-	    touchStartX = touches[0].pageX;
-	    touchStartY = touches[0].pageY;
+			    touchStartX = touches[0].pageX;
+			    touchStartY = touches[0].pageY;
 
-	    document.body.addEventListener('touchmove', handleTouchMove, true);
-	    document.body.addEventListener('touchend', handleTouchEnd, true);
-	    var zoom = document.documentElement.clientWidth / window.innerWidth;
-	    if(zoom < MINIMUM_ZOOM_TO_ENABLE_SCROLL && event.target.id !== "mobile_forward_arrow" && event.target.id !== "mobile_back_arrow"){    	 
-	    	//this is because if not done, the browser can take control of the event and cancels it, 
-	    	//because it thinks that the touch is a scroll action, so we prevent default if the zoom is lower than 1.5, 
-	    	//and there will be no scroll below that zoom level
-	    	event.preventDefault(); 
-	    }
-	  }
+			    document.body.addEventListener('touchmove', handleTouchMove, true);
+			    document.body.addEventListener('touchend', handleTouchEnd, true);
+			    var zoom = document.documentElement.clientWidth / window.innerWidth;
+			    if(zoom < MINIMUM_ZOOM_TO_ENABLE_SCROLL && event.target.id !== "mobile_forward_arrow" && event.target.id !== "mobile_back_arrow"){    	 
+			    	//this is because if not done, the browser can take control of the event and cancels it, 
+			    	//because it thinks that the touch is a scroll action, so we prevent default if the zoom is lower than 1.5, 
+			    	//and there will be no scroll below that zoom level
+			    	event.preventDefault(); 
+			    }
+			  }
+		}
 	};
 
 	var handleTouchMove = function(event) {
-	  var touches = getTouches(event);
-	  if (touches.length > 1) {
-	    cancelTouch();
-	  } else {
-	    touchDX = touches[0].pageX - touchStartX;
-	    touchDY = touches[0].pageY - touchStartY;
-	    var zoom = document.documentElement.clientWidth / window.innerWidth;	  	
-	  	if(zoom < MINIMUM_ZOOM_TO_ENABLE_SCROLL){
-	    	event.preventDefault();  //this is because if not done, the browser can take control of the event and cancels it, because it thinks that the touch is a scroll action
-	  	}
-	  }
-	  
+		if(V.ViewerEngine === "presentation"){
+		  var touches = getTouches(event);
+		  if (touches.length > 1) {
+		    cancelTouch();
+		  } else {
+		    touchDX = touches[0].pageX - touchStartX;
+		    touchDY = touches[0].pageY - touchStartY;
+		    var zoom = document.documentElement.clientWidth / window.innerWidth;	  	
+		  	if(zoom < MINIMUM_ZOOM_TO_ENABLE_SCROLL){
+		    	event.preventDefault();  //this is because if not done, the browser can take control of the event and cancels it, because it thinks that the touch is a scroll action
+		  	}
+		  }
+	    }
 	};
 
 	var handleTouchEnd = function(event) {
-	  var dx = Math.abs(touchDX);
-	  var dy = Math.abs(touchDY);
+		if(V.ViewerEngine === "presentation"){
+		  var dx = Math.abs(touchDX);
+		  var dy = Math.abs(touchDY);
 
-	  if ((dx > PM_TOUCH_SENSITIVITY) && (dy < (dx * 2 / 3))) {
-	    if (touchDX > 0) {
-	      V.Slides.backwardOneSlide();
-	    } else {
-	      V.Slides.forwardOneSlide();
-	    }
-	  }
-	  
-	  cancelTouch();
+		  if ((dx > PM_TOUCH_SENSITIVITY) && (dy < (dx * 2 / 3))) {
+		    if (touchDX > 0) {
+		      V.Slides.backwardOneSlide();
+		    } else {
+		      V.Slides.forwardOneSlide();
+		    }
+		  }
+		  
+		  cancelTouch();
+		}
 	};
 
 	var cancelTouch = function() {
