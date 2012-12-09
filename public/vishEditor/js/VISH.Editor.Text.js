cd39d1f4b55fa6f7a281a9c81f50583f3fce601c
Aldo Gordillo <agordillos@gmail.com>
2012-12-09 16:16:07 +0100

    Improve CKEDITOR integration

--- public/vishEditor/js/VISH.Editor.Text.js
+++ public/vishEditor/js/VISH.Editor.Text.js
@@ -24,14 +24,18 @@ VISH.Editor.Text = (function(V,$,undefined){
   	} else {
   		current_area = $(this).parents(".selectable");
   	}
-
     current_area.attr('type','text');
 
+    //Create the wysiwyg container and add to the area
     var wysiwygContainerId = VISH.Utils.getId();
     var wysiwygContainer = $("<div id='"+wysiwygContainerId+"'></div>")
+    $(wysiwygContainer).attr('style','width: 100%; height: 100%');
     $(current_area).append(wysiwygContainer);
 
+    //Specified CKEditor configuration
     var config = {};
+
+    //Select the features of the toolbar
 	config.toolbar = 'Basic';
 	config.toolbar_Basic =
 	[
@@ -42,14 +46,38 @@ VISH.Editor.Text = (function(V,$,undefined){
 	    ['Font','FontSize'],
 	    ['TextColor','BGColor']
 	];
+
+	//Singleton toolbar
 	config.sharedSpaces =
 	{
 		top : 'toolbar_text'
 	};
+
+	//Disable toolbar expansion
 	config.toolbarCanCollapse = false;
+	//Disable resizing
+	config.resize_enabled = false;
+	//Disable bottom tags
+	config.removePlugins = 'elementspath','resize';
+
+	//Fit the current area
+	config.width = '100%';
+	//The height value defines the height of CKEditor editing area and can be given in pixels or em. Percent values are not supported. 
+	//http://docs.cksource.com/CKEditor_3.x/Howto/Editor_Size_On_The_Fly
+	config.height = $(current_area).height();
+
+	// config.extraPlugins = 'autogrow';
+	// config.autoGrow_maxHeight = $(current_area).height();
+
+	//Apply vEditor skin
 	var ckeditorBasePath = CKEDITOR.basePath.substr(0, CKEDITOR.basePath.indexOf("editor/"));
 	config.skin = 'vEditor,' + ckeditorBasePath + 'editor/skins/vEditor/';
+
+	//Add ckeditor wysiwyg instance
 	var ckeditor = CKEDITOR.appendTo( wysiwygContainerId, config, initial_text );
+	
+	//Catch the focus event
+	//TODO: Improve event cathing... currently is not triggered in all cases.
 	ckeditor.on('focus', function(event){
 		var area = $("div[type='text']").has(event.editor.container.$);
 		VISH.Editor.selectArea(area);
