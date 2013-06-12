e4e7ab0e7886ee95f5a8b371c2743e15bf5de3a4
Aldo Gordillo <agordillos@gmail.com>
2013-06-12 13:30:52 +0200

    Second step for pdf to ex

--- public/vishEditor/js/VISH.Editor.PDFex.js
+++ public/vishEditor/js/VISH.Editor.PDFex.js
@@ -42,7 +42,7 @@ VISH.Editor.PDFex = (function(V,$,undefined){
 			complete: function(xhr) {
 				switch(V.Configuration.getConfiguration()["mode"]){
 					case V.Constant.NOSERVER:
-						var responseTest = '{"urls":["http://localhost:3000//system/pdfexes/attaches/000/000/021/original/vish_user_manual-0.jpg","http://localhost:3000//system/pdfexes/attaches/000/000/021/original/vish_user_manual-1.jpg","http://localhost:3000//system/pdfexes/attaches/000/000/021/original/vish_user_manual-2.jpg","http://localhost:3000//system/pdfexes/attaches/000/000/021/original/vish_user_manual-3.jpg","http://localhost:3000//system/pdfexes/attaches/000/000/021/original/vish_user_manual-4.jpg","http://localhost:3000//system/pdfexes/attaches/000/000/021/original/vish_user_manual-5.jpg","http://localhost:3000//system/pdfexes/attaches/000/000/021/original/vish_user_manual-6.jpg","http://localhost:3000//system/pdfexes/attaches/000/000/021/original/vish_user_manual-7.jpg","http://localhost:3000//system/pdfexes/attaches/000/000/021/original/vish_user_manual-8.jpg","http://localhost:3000//system/pdfexes/attaches/000/000/021/original/vish_user_manual-9.jpg","http://localhost:3000//system/pdfexes/attaches/000/000/021/original/vish_user_manual-10.jpg","http://localhost:3000//system/pdfexes/attaches/000/000/021/original/vish_user_manual-11.jpg","http://localhost:3000//system/pdfexes/attaches/000/000/021/original/vish_user_manual-12.jpg","http://localhost:3000//system/pdfexes/attaches/000/000/021/original/vish_user_manual-13.jpg","http://localhost:3000//system/pdfexes/attaches/000/000/021/original/vish_user_manual-14.jpg","http://localhost:3000//system/pdfexes/attaches/000/000/021/original/vish_user_manual-15.jpg","http://localhost:3000//system/pdfexes/attaches/000/000/021/original/vish_user_manual-16.jpg","http://localhost:3000//system/pdfexes/attaches/000/000/021/original/vish_user_manual-17.jpg","http://localhost:3000//system/pdfexes/attaches/000/000/021/original/vish_user_manual-18.jpg","http://localhost:3000//system/pdfexes/attaches/000/000/021/original/vish_user_manual-19.jpg","http://localhost:3000//system/pdfexes/attaches/000/000/021/original/vish_user_manual-20.jpg","http://localhost:3000//system/pdfexes/attaches/000/000/021/original/vish_user_manual-21.jpg","http://localhost:3000//system/pdfexes/attaches/000/000/021/original/vish_user_manual-22.jpg","http://localhost:3000//system/pdfexes/attaches/000/000/021/original/vish_user_manual-23.jpg","http://localhost:3000//system/pdfexes/attaches/000/000/021/original/vish_user_manual-24.jpg","http://localhost:3000//system/pdfexes/attaches/000/000/021/original/vish_user_manual-25.jpg"]}';
+						var responseTest = '{"urls":["http://localhost:3000/system/pdfexes/attaches/000/000/021/original/vish_user_manual-0.jpg","http://localhost:3000/system/pdfexes/attaches/000/000/021/original/vish_user_manual-1.jpg","http://localhost:3000/system/pdfexes/attaches/000/000/021/original/vish_user_manual-2.jpg","http://localhost:3000/system/pdfexes/attaches/000/000/021/original/vish_user_manual-3.jpg","http://localhost:3000/system/pdfexes/attaches/000/000/021/original/vish_user_manual-4.jpg","http://localhost:3000/system/pdfexes/attaches/000/000/021/original/vish_user_manual-5.jpg","http://localhost:3000/system/pdfexes/attaches/000/000/021/original/vish_user_manual-6.jpg","http://localhost:3000/system/pdfexes/attaches/000/000/021/original/vish_user_manual-7.jpg","http://localhost:3000/system/pdfexes/attaches/000/000/021/original/vish_user_manual-8.jpg","http://localhost:3000/system/pdfexes/attaches/000/000/021/original/vish_user_manual-9.jpg","http://localhost:3000/system/pdfexes/attaches/000/000/021/original/vish_user_manual-10.jpg","http://localhost:3000/system/pdfexes/attaches/000/000/021/original/vish_user_manual-11.jpg","http://localhost:3000/system/pdfexes/attaches/000/000/021/original/vish_user_manual-12.jpg","http://localhost:3000/system/pdfexes/attaches/000/000/021/original/vish_user_manual-13.jpg","http://localhost:3000/system/pdfexes/attaches/000/000/021/original/vish_user_manual-14.jpg","http://localhost:3000/system/pdfexes/attaches/000/000/021/original/vish_user_manual-15.jpg","http://localhost:3000/system/pdfexes/attaches/000/000/021/original/vish_user_manual-16.jpg","http://localhost:3000/system/pdfexes/attaches/000/000/021/original/vish_user_manual-17.jpg","http://localhost:3000/system/pdfexes/attaches/000/000/021/original/vish_user_manual-18.jpg","http://localhost:3000/system/pdfexes/attaches/000/000/021/original/vish_user_manual-19.jpg","http://localhost:3000/system/pdfexes/attaches/000/000/021/original/vish_user_manual-20.jpg","http://localhost:3000/system/pdfexes/attaches/000/000/021/original/vish_user_manual-21.jpg","http://localhost:3000/system/pdfexes/attaches/000/000/021/original/vish_user_manual-22.jpg","http://localhost:3000/system/pdfexes/attaches/000/000/021/original/vish_user_manual-23.jpg","http://localhost:3000/system/pdfexes/attaches/000/000/021/original/vish_user_manual-24.jpg","http://localhost:3000/system/pdfexes/attaches/000/000/021/original/vish_user_manual-25.jpg"]}';
 						processResponse(responseTest);
 					break;
 					case V.Constant.VISH:
@@ -80,8 +80,46 @@ VISH.Editor.PDFex = (function(V,$,undefined){
 	var processResponse = function(response){
 		try  {
 			var jsonResponse = JSON.parse(response);
-			V.Debugging.log(jsonResponse);
+			var presentation = generatePresentationWithImgArray(jsonResponse.urls);
+
+			console.log("presentation");
+			console.log(presentation);
+
+			// $.fancybox.close();
+			V.Editor.Presentation.previewPresentation(presentation);
 		} catch(e) {}
+	}
+
+	var generatePresentationWithImgArray = function(imgs){
+		var presentation = {};
+		presentation.VEVersion = V.VERSION;
+		presentation.type = V.Constant.PRESENTATION;
+		presentation.theme = V.Constant.Themes.Default
+		presentation.slides = [];
+		
+		for(var i=0; i<imgs.length; i++){
+			var imageUrl = imgs[i];
+			presentation.slides.push(_generateSlideWithImg(i,imageUrl));
+		}
+		return presentation;
+	}
+
+	var _generateSlideWithImg = function(index,imgUrl){
+		var slide = {};
+		slide.id = "article"+index;
+		slide.type = V.Constant.STANDARD;
+		slide.template = "t10";
+		slide.elements = [];
+
+		var element = {};
+		element.areaid = "center";
+		element.body = imgUrl;
+		element.id = slide.id + "_zone1";
+		element.style = "position: relative; width:100%; height:100%; top:0%; left:0%;"
+		element.type = V.Constant.IMAGE;
+		slide.elements.push(element);
+
+		return slide;
 	}
 
 	return {
