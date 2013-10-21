fac10b535a553b74e5a68759983c37372cdd256a
Enrique Barra <ebarra@dit.upm.es>
2013-10-21 15:41:01 +0200

    show competitions

--- public/vishEditor/js/VISH.Editor.Tools.Menu.js
+++ public/vishEditor/js/VISH.Editor.Tools.Menu.js
@@ -2,7 +2,7 @@ VISH.Editor.Tools.Menu = (function(V,$,undefined){
 
 	var _initialized = false;
 	var _hoverMenu = false;
-	var _competitionsModalShown = true;
+	var _competitionsModalShown = false;
 
 	/*
 	 * Init singleton
