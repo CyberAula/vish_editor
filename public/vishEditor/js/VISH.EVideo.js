cc9b5be3fa7fc080f34b6873c153ae5f8567015a
Alvaro Sanchez Bravo <a.sanbravo@gmail.com>
2013-11-27 13:11:34 +0100

    Evideo structure created

--- public/vishEditor/js/VISH.EVideo.js
+++ public/vishEditor/js/VISH.EVideo.js
@@ -60,12 +60,44 @@ VISH.EVideo = (function(V,$,undefined){
 		console.log(evideoId);
 	}
 
-	var drawEVideo = function(evideoJSON){
+	var drawEVideo = function(evideoJSON){ // here we have to add the html parsed in Javascript
 		console.log("draw EVideo");
 		console.log(evideoJSON);
 
 		var evideoDOM = $("#"+evideoJSON.id);
-		console.log(evideoDOM);
+
+		var videoBox =  $("<div class='videoBox'></div>");
+		$(evideoDOM).append(videoBox);
+
+		$(videoBox).append($("<div class = 'hide_button2' onClick= 'show()'>x</div>"));
+
+		var videoDiv =  $("<div id = 'videoDiv'></div>");
+		$(videoBox).append(videoDiv);
+		var myvideoId = V.Utils.getId();
+
+		var videoPlayer = $("<video id= '" + myvideoId + "' poster='videos/webvtt_talk.png' style='width:100%' preload='metadata'></video>")
+		$(videoDiv).append(videoPlayer);
+		$(videoPlayer).append("<source src='https://dl.dropboxusercontent.com/u/16070658/html5_video_index/videos/webvtt_talk.webm'></source><source src='videos/webvtt_talk.mp4'></source><track class='nav' src='videos/webvtt_talk_navigation.vtt' kind='chapters' srclang='en'></track><track class='cc' src='videos/webvtt_talk_captions.vtt' kind='captions' label='captions' srclang='en' default></track>");
+
+		
+		var controls = $("<div class='controls'>");
+		$(videoBox).append(controls);
+
+		$(controls).append("<div class='button'><img class='play' src='images/play.png'></img></div>");
+
+		var positionview = $("<div class='positionview'>");
+		$(controls).append(positionview);
+
+
+		var pViewContent = $("<div class='transportbar'><div class='position'></div><div class= 'segmentDiv'></div></div><ul class='segments' title='chapter navigation' aria-describedby='keys'></ul></div><div class='time'><span class='curTime'>00:00</span>/<span class='duration'>00:00</span></div>");
+		$(controls).append(pViewContent);
+		$(videoBox).append("<div style='display: block; clear: both;'></div>");
+
+		var transcriptBox = $("<div class='transcriptBox'></div>");
+		$(videoBox).append(transcriptBox);
+		$(transcriptBox).append("<div id = 'hide_button' onClick= 'hide()''>x</div><div id='navigation' style= 'background-color:#FAF9F8; border:2px black solid;'><ul id='chapters'></ul>")
+	
+		//console.log(evideoDOM);
 	}
 
 	return {
