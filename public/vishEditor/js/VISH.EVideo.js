508bdf2289ee8f5219663640a29ced172a0f2a31
Alvaro Sanchez Bravo <a.sanbravo@gmail.com>
2013-11-28 17:48:33 +0100

    - Deleted showing '0' when the video was less than 1 hour

--- public/vishEditor/js/VISH.EVideo.js
+++ public/vishEditor/js/VISH.EVideo.js
@@ -394,12 +394,22 @@ var _show = function(){
 }
 
 var _secondsTimeSpanToHMS = function(s) {
+	var display;
     var h = Math.floor(s/3600); //Get whole hours
+   
     s -= h*3600;
     var m = Math.floor(s/60); //Get remaining minutes
     s -= m*60;
-    return h+":"+(m < 10 ? '0'+m : m)+":"+(s < 10 ? '0'+s : s); //zero padding on minutes and seconds
+
+     if(h <1){
+     	    display = (m < 10 ? '0'+m : m)+":"+(s < 10 ? '0'+s : s); //zero padding on minutes and seconds
+
+     }else{
+     	display =  h+":"+(m < 10 ? '0'+m : m)+":"+(s < 10 ? '0'+s : s); //zero padding on minutes and seconds
+     }
+    return display;
 }
+
 
 
 
