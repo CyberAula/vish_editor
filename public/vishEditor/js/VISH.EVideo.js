9093ca100ecefb9a31314c209fcbef0f7f78f92c
Alvaro Sanchez Bravo <a.sanbravo@gmail.com>
2013-12-13 13:50:03 +0100

    - Deleted useless CSS classes - Deleted commented lines

--- public/vishEditor/js/VISH.EVideo.js
+++ public/vishEditor/js/VISH.EVideo.js
@@ -27,7 +27,6 @@ VISH.EVideo = (function(V,$,undefined){
 		if(!evideos){
 			evideos = new Array();
 		}
-		// console.log("evideo init");
 	};
 
 	var _loadEvents = function(){
@@ -50,41 +49,7 @@ VISH.EVideo = (function(V,$,undefined){
 	*   (moving at the same time and at the same position) or false if not
 	*/
 	var addBall = function(evideoId, ball){
-		// if((!poi)||(!poi.x)||(!poi.y)||(poi.x > 100)||(poi.y > 100)){
-		// 	//Corrupted poi
-		// 	return;
-		// }
 
-		// var flashcard_div = $("#"+ fcId);
-		// var poiId = V.Utils.getId(fcId + "_poi");
-		// var div_to_add = "<div class='fc_poi' id='" + poiId + "' style='position:absolute;left:"+poi.x+"%;top:"+poi.y+"%'></div>";
-		// flashcard_div.append(div_to_add);
-
-		// if(typeof flashcards[fcId] === "undefined"){
-		// 	flashcards[fcId] = new Object();
-		// 	flashcards[fcId].arrows = [];
-		// }
-
-		// //Add arrow
-		// var arrow = new Object();
-		// arrow.id = poiId;
-		// if(sync){
-		// 	arrow.position = 0;
-		// } else {
-		// 	var rand_pos = Math.floor(Math.random()*TOTAL_FRAMES+1)*FRAME_WIDTH;  //position in pixels
-		// 	arrow.position = rand_pos;
-		// }
-		// arrow.slide_id = poi.slide_id;
-		// arrow.x = poi.x;
-		// arrow.y = poi.y;
-		
-		// flashcards[fcId].arrows.push(arrow);
-		// pois[arrow.id] = arrow;
-
-		// //Add event to the arrow
-		// $("#" + poiId).click(function(event){
-		// 	V.Events.onFlashcardPoiClicked(poiId);
-		// });
 	};
 
 
@@ -100,57 +65,40 @@ VISH.EVideo = (function(V,$,undefined){
 		var timeDiv = $(videoBox).find(".time");
 
 		if(eback == true){
+			var animationTime = 1000;
 			
 			$(currentSlide).find(".segmentDiv").hide();
 			$(currentSlide).find(".segments").hide();
 			$(timeDiv).hide();
-
-
-			var animationTime = 1000;			
 
 			$(hide_button).css("width", hide_buttonW);
 			$(hide_button).css("padding-right", hide_buttonPR);
 			$(hide_button).css("padding-top", hide_buttonPT);
 		
 			nAnimations = 4;
-			$(navigation).animate({width: "0%"}, 1000, function(){ _checkAnimationFinish(); });
-			$(navigation).animate({"padding-right": "0%"},1000, function(){ _checkAnimationFinish(); });
-			$(transcriptBox).animate({width: "5%"}, 1000, function(){ _checkAnimationFinish(); });
-			$(videoBox).animate({width: "92%", "margin-top": "8%"}, 1000, function(){ _checkAnimationFinish(); });
+			$(navigation).animate({width: "0%"}, animationTime, function(){ _checkAnimationFinish(); });
+			$(navigation).animate({"padding-right": "0%"},animationTime, function(){ _checkAnimationFinish(); });
+			$(transcriptBox).animate({width: "5%"}, animationTime, function(){ _checkAnimationFinish(); });
+			$(videoBox).animate({width: "92%", "margin-top": "8%"}, animationTime, function(){ _checkAnimationFinish(); });
 
 
 		}else{
 			$(currentSlide).find(".segmentDiv").hide();
 			$(currentSlide).find(".segments").hide();
 			$(timeDiv).hide();
-			// _show();
 			nAnimations = 4;
-			$(navigation).animate({width: "58%"}, 1000, function(){ _checkAnimationFinish() });
-			$(navigation).animate({"padding-right": "5%"},1000, function(){ _checkAnimationFinish(); });
-			$(transcriptBox).animate({width: "36%"}, 1000, function(){ _checkAnimationFinish(); });
-			$(videoBox).animate({width: "62%", "margin-top": "18%"}, 1000, function(){ _checkAnimationFinish(); });
+			$(navigation).animate({width: "58%"}, animationTime, function(){ _checkAnimationFinish() });
+			$(navigation).animate({"padding-right": "5%"}, animationTime, function(){ _checkAnimationFinish(); });
+			$(transcriptBox).animate({width: "36%"}, animationTime, function(){ _checkAnimationFinish(); });
+			$(videoBox).animate({width: "62%", "margin-top": "18%"}, animationTime, function(){ _checkAnimationFinish(); });
 		}
-
- // Utilizar el contador para hacer el hide cuando hayan acabado todas las animaciones.
-
-		// $( ".navigation" ).animate({
-		// width: "0%"
-		// }, 1000, function() {
-		// 	var transcriptBox = $(V.Slides.getCurrentSlide()).find(".transcriptBox");
-		// 	$(transcriptBox).find(".navigation").css("padding-right", "0%");
-		// 	$(transcriptBox).find(".hide_button").css("width","100%");
-		// 	$(transcriptBox).css("width","5%")
-		// 	_hide();
-		// });
-		// }else{
-		// 	_show();
-		// }
 	}
 
 	var nAnimationsFinished = 0;
 	var nAnimations;
+
+
 	var _checkAnimationFinish = function(){
-		console.log("_checkAnimationFinish called");
 		nAnimationsFinished = nAnimationsFinished +1;
 		if(nAnimationsFinished===nAnimations){
 			_onAnimationsFinish();
@@ -172,8 +120,6 @@ VISH.EVideo = (function(V,$,undefined){
 	}
 
 	var drawEVideo = function(evideoJSON){ // here we have to add the html parsed in Javascript
-		// console.log("draw EVideo");
-		// console.log(evideoJSON);
 
 		var evideoDOM = $("#"+evideoJSON.id);
 
@@ -205,15 +151,8 @@ VISH.EVideo = (function(V,$,undefined){
 		$(transcriptBox).append("<div class='hide_button'><div class= 'i_letter'>I\n</div><div class='inside_hide'>N\nD\nE\nX</div><div class='eback_button'><div><</div></div></div></div><div class='navigation'><ul class='chapters'></ul>")
 	
 		video = $('#' + myvideoId)[0];
-		// console.log("video");
-		// console.log(video);
-
-
 
 		//Vars
-
-		// console.log("duracion");
-		// console.log(duracion);
 
 
 		$(video).on("loadeddata", function(data){
@@ -225,12 +164,10 @@ VISH.EVideo = (function(V,$,undefined){
 			//http://www.w3schools.com/tags/av_prop_readystate.asp
 
 			if((video.readyState == 4)||(video.readyState == 3)){
-				_init(video,evideoJSON);
 				var t = _secondsTimeSpanToHMS(video.duration.toFixed(0));
-				//console.log("variable t: ");
-				//console.log(t);
+				_init(video,evideoJSON);
 				$(V.Slides.getCurrentSlide()).find(".duration").html(t);
-			duracion = video.duration.toFixed(2);
+				duracion = video.duration.toFixed(2);
 			} else {
 				console.log("Video not loaded appropriately");
 			}
@@ -255,12 +192,7 @@ var _init = function(video,evideoJSON){
    	_getNextBall(0);
 	_paintBalls();
 	_paintIndex();
-
-
-
-}
-		//console.log(evideoDOM);
-
+	}
 }
 
 var _renderVideo = function(){
@@ -269,29 +201,27 @@ var _renderVideo = function(){
 	}else{
 		//Aquí se renderizarán los vídeos de Youtube
 	}
-
 }
 
 
 var _renderHTML5Video = function(){
-	myvideoId = V.Utils.getId();	
+	myvideoId = V.Utils.getId();
+
+	//var videoPlayer = $("<iframe src='//www.youtube.com/embed/nlcIKh6sBtc' frameborder='0'  style='width:100%; 	height: 100% !important;' allowfullscreen></iframe>")
 	var videoPlayer = $("<video id= '" + myvideoId + "' poster='images/videos/webvtt_talk.png' style='width:100%; 	height: 100% !important;'  preload='metadata'></video>")
 	$(videoDiv).append(videoPlayer);
 	$(videoPlayer).append("<source src='https://dl.dropboxusercontent.com/u/16070658/html5_video_index/videos/webvtt_talk.webm'></source><source src='images/videos/webvtt_talk.mp4'></source><track class='nav' src='images/videos/webvtt_talk_navigation.vtt' kind='chapters' srclang='en'></track><track class='cc' src='images/videos/webvtt_talk_captions.vtt' kind='captions' label='captions' srclang='en' default></track>");
 }
 
+var _onClickPlayVideo = function(evt){
+	video= _getCurrentEVideo();
+	_togglePlay(video);
+}
 
-	var _onClickPlayVideo = function(evt){
-		console.log("se ejecuta _onClickPlayVideo");
-		video= _getCurrentEVideo();
-		_togglePlay(video);
-	}
-
-	var _togglePlay = function(video){
-		console.log("entro en _togglePlay");
-		video = _getCurrentEVideo();
-		if (video.paused == false) {
-			video.pause();
+var _togglePlay = function(video){
+	video = _getCurrentEVideo();
+	if (video.paused == false) {
+		video.pause();
 			$(V.Slides.getCurrentSlide()).find(".play").attr("src","images/evideo/play.png");
 		} else {
 			video.play();
@@ -302,95 +232,92 @@ var _renderHTML5Video = function(){
 
 
 
-	var _onClickTransportBar = function(evt){
+var _onClickTransportBar = function(evt){
 
-		var video = _getCurrentEVideo();
-		console.log("SEEK");
-		console.log(video);
+	var video = _getCurrentEVideo();
 
-		//this ahora es evt.target
 
-		var bar_width = $('.positionview').outerWidth();
-		//var bar_width = document.getElementById('positionview').offsetWidth; // "!it calculates the size of bar_width dinamically, even when you resize the navigation window.
-		// console.log("bar_width: ");
-		// console.log(videon);
-		var offset = $(evt.target).offset();
-		var distanceDifference = evt.pageX - offset.left; //eliminar número mágico
-		var percentWidth = ((distanceDifference*100) / bar_width)*0.99;
-		video.currentTime = (percentWidth * video.duration) / 100;
+	var bar_width = $('.positionview').outerWidth();
+	var offset = $(evt.target).offset();//this ahora es evt.target
+	var distanceDifference = evt.pageX - offset.left; //eliminar número mágico
+	var percentWidth = ((distanceDifference*100) / bar_width)*0.99;
+	video.currentTime = (percentWidth * video.duration) / 100;
 
-		nextBall = null;
-		_getNextBall(video.currentTime);
-		_resetShowParams();
+	nextBall = null;
+	_getNextBall(video.currentTime);
+	_resetShowParams();
+}
+
+
+
+var _getNextBall = function(time){
+	if(typeof time != "number"){
+		return null;
 	}
 
-
-
-	var _getNextBall = function(time){
-			if(typeof time != "number"){
-				return null;
+	var myNextball;
+	var pTime = video.duration;
+	
+	for (i = 0; i < balls.length; i++) {
+		var ball = balls[i];
+		var ballTime = parseFloat(ball.time);
+		if((ballTime < pTime)&&(ballTime > 0)&&(ballTime > time)){
+			if(ball != nextBall){
+				myNextball = ball;
+				break;
 			}
-
-			var myNextball;
-			var pTime = video.duration;
-			for (i = 0; i < balls.length; i++) {
-				var ball = balls[i];
-				var ballTime = parseFloat(ball.time);
-				if((ballTime < pTime)&&(ballTime > 0)&&(ballTime > time)){
-					if(ball != nextBall){
-						myNextball = ball;
-						break;
-					}
-				}
-			}
-			if(myNextball){
-				nextBall = myNextball;
-			} else {
-				nextBall = undefined;
-			}
-			return nextBall;
-	}
-
-
-
-	var _resetShowParams = function(){
-		$(balls).each(function(index,ball){
-			ball.showed = false;
-		});	
-	}
-
-	var _popUp = function(callback, ball){
-		var video = _getCurrentEVideo();
-		if (video.paused == false) {
-			state_vid = true;
-		}else{
-			state_vid = false;
 		}
+	}
+			
+	if(myNextball){
+		nextBall = myNextball;
+		}else{
+		nextBall = undefined;
+		}
+			return nextBall;
+}
+
+
+
+var _resetShowParams = function(){
+	$(balls).each(function(index,ball){
+		ball.showed = false;
+	});	
+}
+
+
+
+var _popUp = function(callback, ball){
+	var video = _getCurrentEVideo();
+	if (video.paused == false) {
+		state_vid = true;
+	}else{
+		state_vid = false;
+	}
 		video.pause();
 		var slide_id = ball.slide_id;
 		ball.showed = true;
 
-
 		V.Slides.openSubslide(slide_id,true);
-	}
+}
 
-	var _onCloseSubslide = function(subslide_id){
-		var video = $('#' + myvideoId)[0];
-		var prevNextBall = nextBall;
-		_getNextBall(video.currentTime);
-		if(nextBall-prevNextBall < RANGE){
-			_popUp(_onCloseSubslide,nextBall);		
-		} else {
-			if(state_vid == true){
-			console.log("state_vid == true");
+
+
+var _onCloseSubslide = function(subslide_id){
+	var video = $('#' + myvideoId)[0];
+	var prevNextBall = nextBall;
+	_getNextBall(video.currentTime);
+	if(nextBall-prevNextBall < RANGE){
+		_popUp(_onCloseSubslide,nextBall);		
+	}else{
+		if(state_vid == true){
 			video.play(); // we have to know the previous state of the video
 			$(V.Slides.getCurrentSlide()).find(".play").attr("src","images/evideo/pause.png");
 		}else{
-			console.log("state_vid == false");
 			video.pause();
 		}
 	}
-	}
+}
 
 var _getBalls = function(evideoJSON){
 	for (i = 0; i < evideoJSON.pois.length; i++) {
@@ -423,12 +350,16 @@ var _paintIndex = function(){
 		$(V.Slides.getCurrentSlide()).find(".chapters").append(item);
 	}
 
-		$(V.Slides.getCurrentSlide()).find(".chapters li a").on("click", function(event){
-		video.currentTime = $(event.target).attr("time");
-		$(V.Slides.getCurrentSlide()).find(".play").attr("src","images/evideo/play.png");
-	    _popUp(_onCloseSubslide,balls[parseInt($(event.target).attr("ident"))]); //migrated to JQuery working
+	$(V.Slides.getCurrentSlide()).find(".chapters li a").on("click", function(event){
+	video.currentTime = $(event.target).attr("time");
+	$(V.Slides.getCurrentSlide()).find(".play").attr("src","images/evideo/play.png");
+    _popUp(_onCloseSubslide,balls[parseInt($(event.target).attr("ident"))]); //migrated to JQuery working
 	});
 }
+
+
+
+
 var _videoPlayPause = function(evt) {
 	var video = _getCurrentEVideo();
 	if (evt.keyCode == "32") { // space bar
@@ -486,22 +417,11 @@ var _curTimeUpdate = function(evt) {
 	$(V.Slides.getCurrentSlide()).find(".curTime").html(tiemph);
 	
 	var percentWidth = (100*video.currentTime)/video.duration;
-	// var wid = (Math.round(bar_width*video.currentTime/video.duration) * 100)/($('.transportbar').width()) + "%";
-	//console.log("percentWidth");
-	//console.log(percentWidth);
 	$(V.Slides.getCurrentSlide()).find(".position").css("width", percentWidth + "%"); //for the html to draw
 }
 
 var _hide = function() {
 	currentSlide = $(V.Slides.getCurrentSlide());
-	/*$(V.Slides.getCurrentSlide()).find(".videoBox").css("width", '92%');
-	$(V.Slides.getCurrentSlide()).find(".videoBox").css("margin-top", '8%');
-	$(V.Slides.getCurrentSlide()).find(".videoDiv").css("width", '92%');
-	$(V.Slides.getCurrentSlide()).find(".videoDiv").css("height", 'auto');
-	$(V.Slides.getCurrentSlide()).find(".controls").css("width", '92%');
-	$(V.Slides.getCurrentSlide()).find(".time").css("font-size", '1.6rem');
-	$(V.Slides.getCurrentSlide()).find(".time").css("margin-bottom", '1%');
-	$(V.Slides.getCurrentSlide()).find(".time").css("margin-right", '8%');*/
 	$(currentSlide).find(".time").show();
 	$(currentSlide).find(".time").css("font-size", '1.6rem');
 	$(currentSlide).find(".segmentDiv").show();
@@ -527,17 +447,6 @@ var _show = function(){
 	_eraseBalls();
 	_paintBalls();
 	$(V.Slides.getCurrentSlide()).find(".ball").css("margin-top", '5%');
-	//$(V.Slides.getCurrentSlide()).find(".videoBox").css("width", '55%');
-	//_eraseBalls();
-	//_paintBalls();
-	//$(V.Slides.getCurrentSlide()).find(".navigation").show();
-	//$(V.Slides.getCurrentSlide()).find(".ball").css("margin-top", '5%');
-	//$(V.Slides.getCurrentSlide()).find(".hide_button").css("margin-left", "-4%");
-	//$(V.Slides.getCurrentSlide()).find(".transcriptBox").css("width", "36%");
-	//$(V.Slides.getCurrentSlide()).find(".videoBox").css("margin-top", '18%');
-	//$(V.Slides.getCurrentSlide()).find(".time").css("font-size", '1rem');
-	//$(V.Slides.getCurrentSlide()).find(".time").css("margin-bottom", 'auto');
-	//$(V.Slides.getCurrentSlide()).find(".transcriptBox").show();
 	eback = true;
 }
 
