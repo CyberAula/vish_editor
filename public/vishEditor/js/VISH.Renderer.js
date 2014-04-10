8b0a4dcc6371ed3fb7d6b8926b1143d4919ba10f
Aldo Gordillo <agordillos@gmail.com>
2014-04-10 12:10:19 +0200

    Render SCORM Packages in ViSH Viewer with the SCORM module

--- public/vishEditor/js/VISH.Renderer.js
+++ public/vishEditor/js/VISH.Renderer.js
@@ -141,6 +141,9 @@ VISH.Renderer = (function(V,$,undefined){
 			case V.Constant.MEDIA.YOUTUBE_VIDEO:
 				return V.Video.Youtube.renderVideoFromJSON(element,{extraClasses: "objectelement youtubeelement " + template+"_"+ element['areaid']});
 				break;
+			case V.Constant.MEDIA.SCORM_PACKAGE:
+				return V.SCORM.renderSCORMFromJSON(element,{extraClasses: "" + template +"_" + element['areaid']});
+				break;
 			default:
 				var style = (element['style'])? element['style'] : "";
 				var body = element['body'];
