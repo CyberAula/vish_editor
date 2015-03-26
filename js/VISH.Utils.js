462d8182ae8888e85b859af8052ec6708060a1c6
agordillo <agordillos@gmail.com>
2015-03-26 11:44:06 +0100

    Bug fixed on evideos and animations

--- js/VISH.Utils.js
+++ js/VISH.Utils.js
@@ -57,33 +57,6 @@ VISH.Utils = (function(V,undefined){
 			};
 		// };
 
-		if(!Array.prototype.filter){
-			Array.prototype.filter = function(fun /*, thisp */){
-				"use strict";
-
-				if (this == null)
-					throw new TypeError();
-
-				var t = Object(this);
-				var len = t.length >>> 0;
-				if (typeof fun != "function")
-					throw new TypeError();
-
-				var res = [];
-				var thisp = arguments[1];
-				for (var i = 0; i < len; i++)
-				{
-					if (i in t)
-					{
-						var val = t[i]; // in case fun mutates this
-						if (fun.call(thisp, val, i, t))
-							res.push(val);
-					}
-				}
-				return res;
-			};
-		};
-
 		//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
 		if(!Array.prototype.map){
 			Array.prototype.map = function(fun /*, thisArg */){
