ec9f229fb47626200ce58bd08409cf80d0d92d31
agordillo <agordillos@gmail.com>
2015-05-26 15:29:00 +0200

    Minor changes

--- js/VISH.Editor.Object.Flash.js
+++ js/VISH.Editor.Object.Flash.js
@@ -1,33 +1,33 @@
 VISH.Editor.Object.Flash = (function(V,$,undefined){
 	
 	var drawFlashObjectWithSource = function(src){
-	  var current_area = V.Editor.getCurrentArea();
-	  var template = V.Editor.getTemplate(current_area);
+		var current_area = V.Editor.getCurrentArea();
+		var template = V.Editor.getTemplate(current_area);
 
-	  var nextFlashId = V.Utils.getId();
-	  var idToDrag = "draggable" + nextFlashId;
-	  var idToResize = "resizable" + nextFlashId;
-	  current_area.attr('type','object');
-	   
-	  var embedDiv = document.createElement('div');
-	  embedDiv.setAttribute('id', idToDrag);
-	  $(embedDiv).addClass('object_wrapper');
-	  $(embedDiv).addClass(template + "_object");
-	  
-	  var embedTag = document.createElement('embed');
-	  embedTag.setAttribute('id', idToResize );
-	  embedTag.setAttribute('class', template + "_object");
-	  src = V.Utils.addParamToUrl(src,"wmode","opaque");
-	  embedTag.setAttribute('src', src);
-	  embedTag.setAttribute('wmode', 'opaque');
-	  $(embedDiv).append(embedTag); 
-	  
-	  $(current_area).html("");
-	  $(current_area).append(embedDiv);
-	  	    
-	  V.Editor.addDeleteButton($(current_area));
-	  
-	  $("#" + idToDrag).draggable({cursor: "move"});
+		var nextFlashId = V.Utils.getId();
+		var idToDrag = "draggable" + nextFlashId;
+		var idToResize = "resizable" + nextFlashId;
+		current_area.attr('type','object');
+
+		var embedDiv = document.createElement('div');
+		embedDiv.setAttribute('id', idToDrag);
+		$(embedDiv).addClass('object_wrapper');
+		$(embedDiv).addClass(template + "_object");
+
+		var embedTag = document.createElement('embed'); //should we use iframe instead of embed?
+		embedTag.setAttribute('id', idToResize );
+		embedTag.setAttribute('class', template + "_object");
+		src = V.Utils.addParamToUrl(src,"wmode","opaque");
+		embedTag.setAttribute('src', src);
+		embedTag.setAttribute('wmode', 'opaque');
+		$(embedDiv).append(embedTag); 
+
+		$(current_area).html("");
+		$(current_area).append(embedDiv);
+			    
+		V.Editor.addDeleteButton($(current_area));
+
+		$("#" + idToDrag).draggable({cursor: "move"});
 	}
 			
 	return {
